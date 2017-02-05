1927c7bc27bc79a6f10d9e957ce472edce37d2c2
Ewelina Dziewiela <ewelinadziewiela@MacBook-Pro-Ewelina.local>
2017-02-05 15:11:39 +0100

    style and js fix

--- application.js
+++ application.js
@@ -3,3 +3,19 @@ $('body').scrollspy({target: ".navbar"});
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+$('.show-more_btn').click(function() {
+  $(".show-more_mobile").toggleClass("show-less_mobile");
+  if ($(this).text() === "Show more") {
+    $(this).text("Show less")
+  }
+  else {
+    $(this).text("Show more")
+  }
+});
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+  if( $(e.target).is('a') ) {
+    $(this).collapse('hide');
+  }
+});
